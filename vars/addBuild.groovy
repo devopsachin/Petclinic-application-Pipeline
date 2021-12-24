ceee15216e995dc2aca0813f35b798fab88a650e
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-24 17:14:40 +0530

    Update addBuild.groovy

--- vars/addBuild.groovy
+++ vars/addBuild.groovy
@@ -1,12 +1,5 @@
-class Deployer implements Serializable {
-   def steps
-   Deployer(steps) {
-      this.steps = steps
-   }
-
     def callMe() {
         steps.echo("Test")
         steps.echo("${steps.env.BRANCH_NAME}")
         steps.sh("ls -al")
     }
-}
