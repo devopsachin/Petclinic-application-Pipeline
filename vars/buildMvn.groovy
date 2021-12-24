cacc1ceb54f63e7571d9ddbb012140fe68131707
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-24 17:04:32 +0530

    Update buildMvn.groovy

--- vars/buildMvn.groovy
+++ vars/buildMvn.groovy
@@ -1,5 +1,6 @@
 def call (Map stageParams){
  path: [[dir: stageParams.path ]],
  command: [[cmd: stageParams.command]]
- sh ('''cd $path && ./mvnw $command''')  
+ steps.sh("cd ${path}")
+ steps.sh("./mvnw ${cmd}")
 }
