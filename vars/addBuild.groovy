abea8b644adef9e878d95f2424ac6ec337c6987f
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-24 17:35:12 +0530

    Update addBuild.groovy

--- vars/addBuild.groovy
+++ vars/addBuild.groovy
@@ -1,4 +1,12 @@
-def call (String path = 'none'){
-    echo "cd ${path}",
-    echo "./mvnw package"
+public class SharedLibrary {
+  def pipeline
+  
+  public SharedLibrary(pipeline) {
+    pipeline.steps.echo(pipeline.getClass().toString())
+    this.pipeline = pipeline
+  }
+  
+  public void startBuild() {
+    pipeline.steps.sh "echo 'Hello, World!'"
+  }
 }
