fe260d8fca059b0db811bd1de6ca402516e7847a
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-24 16:32:59 +0530

    updated git clone library

--- vars/pullGit.groovy
+++ vars/pullGit.groovy
@@ -1,1 +1,7 @@
-def 
+def call (Map stageParams){
+  checkout ([
+           $class: 'GitSCM',
+            branches: [[name: stageParams.branch ]],
+            userRemoteConfigs: [[url: stageParams.Url]]
+    ])
+]
