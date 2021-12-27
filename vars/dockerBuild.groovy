36eb7685168215de89fa7d5991f09ed3fd7ed557
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-27 16:17:13 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,10 +1,11 @@
 def login (){
-    withCredentials(
-    [
-    usernamePasswordMultiBinding(credentialsId: 'docker-hub', 
-    usernameVarriable: 'dockeruser', 
-    passwordVarriable: 'dockerpassword'
-    )]){
-      sh """docker login --username="${dockeruser}" --password="${dockerpassword}" """
-   }
+withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'docker-hub',
+                    usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+  sh """sudo docker login --username="${USERNAME}" --password="${PASSWORD}" """
   }
+}
+def build(String tag){
+    sh """ sudo docker build -f Dockerfile -t "${tag}" """
+}
+
+        
