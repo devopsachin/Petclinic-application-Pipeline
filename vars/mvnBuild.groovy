6d0f5bd8e57521fb6e74930956463cbcb35327a7
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-27 10:48:43 +0530

    Update mvnBuild.groovy

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,12 +1,7 @@
 def call (String command){
-  pipeline {
-      agent any 
-    stages {
       stage ('Cleaning and Building repo'){
         steps{
         sh "mvn ${command} "
       }
       }
     }
-  }
-}
