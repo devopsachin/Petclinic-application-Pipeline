0ba3b249904fe8eca3530aa6e4924d051d9e961e
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-27 10:42:29 +0530

    Update mvnBuild.groovy

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,10 +1,6 @@
 def call (String command){
   pipeline {
       agent any 
-    tools {
-        maven 'Maven 3.5.0'
-        jdk 'jdk8'
-    }
     stages {
       stage ('Cleaning and Building repo'){
         steps{
