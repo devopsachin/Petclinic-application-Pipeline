ea37a273cf3e7172fdcf24fadf86d8a33b4f7197
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-28 09:57:56 +0530

    Update remoteSSH.groovy

--- vars/remoteSSH.groovy
+++ vars/remoteSSH.groovy
@@ -1,6 +1,6 @@
 def ssh(def username, def ipAdr){
     sshagent (credentials: ['ssh-key']){
-      sh """ssh -o StrictHostKeyChecking=no "${username}"@"${ipAdr}" whoami"""
-      sh """whoami"""
+      sh """ssh -o StrictHostKeyChecking=no "${username}"@"${ipAdr}" """
+      sh """ ssh "${username}"@"${ipAdr}" echo $DOCKERHUB_CREDENTIALS_PSW | sudo docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin """
     }
 }
