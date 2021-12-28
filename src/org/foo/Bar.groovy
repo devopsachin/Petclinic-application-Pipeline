ec173f9b9183c71e997aadc024d28ed9bff5a2c9
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-28 15:27:40 +0530

    Update Bar.groovy

--- src/org/foo/Bar.groovy
+++ src/org/foo/Bar.groovy
@@ -3,6 +3,7 @@ class Bar implements Serializable {
   def steps
   Bar(steps) {this.steps = steps}
   def mvn(args) {
-    steps.sh "${steps.tool 'mvn'}/bin/mvn -o ${args}"
+   // steps.sh "${steps.tool 'mvn'}/bin/mvn -o ${args}"
+    steps.sh "echo Hello!"
   }
 }
