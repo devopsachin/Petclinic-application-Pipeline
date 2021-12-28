0e7f26a4315d783b8458e8a3fa9b098db2eadc5a
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-28 09:18:46 +0530

    Update remoteSSH.groovy

--- vars/remoteSSH.groovy
+++ vars/remoteSSH.groovy
@@ -1,6 +1,5 @@
 def ssh(def username, def ipAdr){
-    withCredentials([[$class: 'ssh-agent', credentialsId: 'ssh-key',
-                      usernameVariable: 'USERNAME' ]]){
+    withCredentials([sshUserPrivateKey(credentialsId: 'ssh-key', keyFileVariable: 'keyfile')]){
       sh """ssh StrictHostKeyChecking=no "${username}"@"${ipAdr}" """
     }
 }
