4c035bf3c8ef481cdf4576abd519c233dc835b65
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-28 15:50:01 +0530

    Update dockerBuild.groovy

--- src/org/foo/dockerBuild.groovy
+++ src/org/foo/dockerBuild.groovy
@@ -6,7 +6,7 @@ class dockerBuild implements Serializable {
   def login (){
   withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'docker-hub',
                     usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-  steps.sh """ docker login --username=admin --password= """
+  login.steps.sh """ docker login --username=admin --password= """
   //sudo docker login --username="${USERNAME}" --password="${PASSWORD}" """
   }
 }
