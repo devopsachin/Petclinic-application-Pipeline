153cfac0e4ba95271b7ed23c06cb7e8cf9f56419
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-29 12:00:40 +0530

    Update Ub.groovy

--- src/org/foo/Ub.groovy
+++ src/org/foo/Ub.groovy
@@ -6,7 +6,7 @@ class U implements Serializable {
     def doArchiveToNexus(String credentials){
         try {
             this.steps.withCredentials([steps.usernameColonPassword(credentialsId: credentials, variable: 'JENKINS_USER')]) {
-                this.steps.sh "sudo docker login --username " + '${JENKINS_USER}' + " --password mydocker2021"
+                this.steps.sh "sudo docker login --username " + '${JENKINS_USER}' + " --password "mydocker2021" "
             }
         } catch (error){
             this.steps.echo error.getMessage()
