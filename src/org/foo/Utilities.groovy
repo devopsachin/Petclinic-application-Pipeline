35f3a56d851db1b0bd1926225a50d68e19a3c784
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-29 11:43:36 +0530

    Create Utilities.groovy

---
+++ src/org/foo/Utilities.groovy
@@ -0,0 +1,17 @@
+package org.foo
+
+class Utilities implements Serializable {
+def steps
+Utilities(steps) {
+    this.steps = steps
+}
+def execute() {
+    this.steps.withCredentials(
+            bindings: [
+                this.steps.usernameColonPassword(
+                    credentialsId: this.docker-hub, 
+                    variable: "unameColonPwd")
+            ]) {
+        this.steps.sh "echo {this.steps.env.unameColonPwd}"
+    }
+}
