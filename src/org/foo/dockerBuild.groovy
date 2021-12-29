8c5debf94c2f68864323b20c94e76bdb3d90c151
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-29 11:34:47 +0530

    Update dockerBuild.groovy

--- src/org/foo/dockerBuild.groovy
+++ src/org/foo/dockerBuild.groovy
@@ -7,9 +7,9 @@ class dockerBuild implements Serializable {
   }
   def login(String credentials){
     try{
-      this.steps.withCredentials([steps.UsernamePasswordMultiBinding(credentialsId: credentials, variable: 'usernameVariable' )]){
+      this.steps.withCredentials([steps.usernameColonPassword(credentialsId: credentials, variable: 'usernameVariable' )]){
                                                               
-        steps.sh "sudo docker login --username '${usernameVariable}' --password "
+        this.steps.sh "sudo docker login --username '${usernameVariable}' --password "
         }
       }catch (error){
             this.steps.echo error.getMessage()
