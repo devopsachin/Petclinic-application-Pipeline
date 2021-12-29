6716da434c971dc4eafdecbaaff1b3e5429ecc19
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-29 12:19:35 +0530

    Update Ub.groovy

--- src/org/foo/Ub.groovy
+++ src/org/foo/Ub.groovy
@@ -8,7 +8,7 @@ class U implements Serializable {
             this.steps.withCredentials([steps.usernamePassword(credentialsId: credentials, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD' )]) {
               //  this.steps.echo " + '${JENKINS_USER}' + "
                // this.steps.sh "sudo docker login --username " + '${USERNAME}' + " --password " + '${PASSWORD}' + " "
-                this.steps.sh "sudo docker login --username '${USERNAME}' --password '${PASSWORD}'
+                this.steps.sh "sudo docker login --username '${USERNAME}' --password '${PASSWORD}' "
             }
         } catch (error){
             this.steps.echo error.getMessage()
