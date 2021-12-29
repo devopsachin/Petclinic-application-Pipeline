9ab54972e3a65da5482d8f83d4223adc02f7ee3c
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-29 09:15:50 +0530

    Create shell.groovy

---
+++ src/org/foo/shell.groovy
@@ -0,0 +1,10 @@
+import org.foo
+class shell implements  Serializable{
+  def steps
+  shell (steps){
+    this.steps = steps
+  }
+  def script(){
+    steps.sh "sudo docker login --user username --password password"
+  }
+}
