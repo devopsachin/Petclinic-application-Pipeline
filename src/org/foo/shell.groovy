926611cf5c3e9dab8a8b4737c876aac541f74a8e
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-29 09:28:56 +0530

    Update shell.groovy

--- src/org/foo/shell.groovy
+++ src/org/foo/shell.groovy
@@ -4,7 +4,7 @@ class shell implements Serializable {
   shell(steps) {this.steps = steps}
   def mvn(args) {
    // steps.sh "${steps.tool 'mvn'}/bin/mvn -o ${args}"
-    steps.sh "docker login --username ${args}"
+    steps.sh "sudo docker login --username ${args} --password mydocker2021"
   }
 }
 
