1175fde7128287a228c81b1c02504c7f0ad2a6ec
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-29 13:59:57 +0530

    Update Ub.groovy

--- src/org/foo/Ub.groovy
+++ src/org/foo/Ub.groovy
@@ -7,7 +7,7 @@ class Docker implements Serializable {
       //  try {
             this.steps.withCredentials([steps.usernamePassword(credentialsId: credentials, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD' )]) 
             {
-                this.steps.sh "echo " + '${PASSWORD}' + " | sudo docker login -u " + '${usernameVariable}' + " --password-stdin "
+                step.this.steps.sh "sudo docker login -u " + '${usernameVariable}' + " --password " + '${PASSWORD}' + " "
             }
      //   } 
      //   catch (error){
