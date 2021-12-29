1bb5dfad0e76d63b7f63830ef8c99e2881440491
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-29 11:04:58 +0530

    Update dockerBuild.groovy

--- src/org/foo/dockerBuild.groovy
+++ src/org/foo/dockerBuild.groovy
@@ -6,7 +6,7 @@ class dockerBuild implements Serializable {
     this.steps = steps
   }
   def login(path, dockerRepoName, applicationName, USERNAME, PASSWORD){
-    steps.sh "sudo docker login --user $USERNAME --password $PASSWORD"
+    steps.sh "sudo docker login --username $USERNAME --password $PASSWORD"
     steps.sh "sudo docker build -f ${path}/Dockerfile -t ${dockerRepoName}/${applicationName} ."
     steps.sh "sudo docker push ${dockerRepoName}/${applicationName} "
       }
