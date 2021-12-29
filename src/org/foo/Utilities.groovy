f57a36cc82c79ccd0f0a9ecd297d3f1284daf140
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-29 11:52:04 +0530

    Update Utilities.groovy

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -1,18 +1,16 @@
-package org.foo
-
 class Utilities implements Serializable {
-def steps
-    Utilities(steps){    
-    this.steps = steps
-}
-def login() {
-    this.steps.withCredentials(
-            bindings: [
-                this.steps.usernameColonPassword(
-                    credentialsId: this.docker-hub, 
-                    variable: "unameColonPwd")
-            ]) {
-        this.steps.sh "echo {this.steps.env.unameColonPwd}"
+    def steps
+    Utilities(steps) {
+        this.steps = steps
     }
-}
+    def doArchiveToNexus(String credentials, String artifact, String artifact_registry_path){
+        try {
+            this.steps.withCredentials([steps.usernameColonPassword(credentialsId: credentials, variable: 'JENKINS_USER')]) {
+                this.steps.sh "curl --user " + '${JENKINS_USER}' + " --upload-file ${artifact} ${artifact_registry_path}"
+            }
+        } catch (error){
+            this.steps.echo error.getMessage()
+            throw error
+        }
+    }
 }
