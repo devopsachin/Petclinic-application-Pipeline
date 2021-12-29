c5d0636547ed9ccee8a48f2c56177dd34d9589b4
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-29 12:24:25 +0530

    Update Ub.groovy

--- src/org/foo/Ub.groovy
+++ src/org/foo/Ub.groovy
@@ -1,18 +1,25 @@
-class U implements Serializable {
+class Docker implements Serializable {
     def steps
-    U(steps) {
+    Docker(steps) {
         this.steps = steps
     }
-    def doArchiveToNexus(String credentials){
+    def dockerLogin(String credentials){
         try {
-            this.steps.withCredentials([steps.usernamePassword(credentialsId: credentials, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD' )]) {
-              //  this.steps.echo " + '${JENKINS_USER}' + "
-               // this.steps.sh "sudo docker login --username " + '${USERNAME}' + " --password " + '${PASSWORD}' + " "
-                this.steps.sh "sudo docker login --username '${USERNAME}' --password '${PASSWORD}' "
+            this.steps.withCredentials([steps.usernamePassword(credentialsId: credentials, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD' )]) 
+            {
+                this.steps.sh "sudo docker login --username " + '${USERNAME}' + " --password " + '${PASSWORD}' + " "
             }
-        } catch (error){
+        } 
+        catch (error){
             this.steps.echo error.getMessage()
             throw error
         }
     }
+    def dockerBuild(path, dockerRepoName, applicationName){
+        this.step.sh "sudo docker build -f ${path}/Dockerfile -t ${dockerRepoName}/${applicationName} ."
+    }
+    def dockerPush(path, dockerRepoName, applicationName){
+        this.steps.sh "sudo docker push ${dockerRepoName}/${applicationName}"
+    }
+        
 }
