500daff1187bf80add8b06b205b5d79ba82fb003
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-12-30 11:43:53 +0530

    Update sonarQube.groovy

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -3,7 +3,7 @@ def test(def projectkey){
     withSonarQubeEnv('sonarqube'){
     sh "${scannerHome}/bin/sonar-scanner \
     -Dsonar.projectKey=${projectkey} \
-        -Dsonar.working.directory=${WORKSPACE} \
+        -Dsonar.working.directory= \
       -Dsonar.exclusions=vendor/**,resources/**,**/*.java " 
  }
 }
