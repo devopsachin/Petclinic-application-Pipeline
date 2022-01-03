08ec17fa63c5dd45eb4c4d5546e1cdf3491719cc
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2022-01-03 14:39:30 +0530

    Update otherRemote.groovy

--- vars/otherRemote.groovy
+++ vars/otherRemote.groovy
@@ -21,8 +21,11 @@ def kubessh(def username, def ipAdr, def applicationName, def dockerRepo, def ap
         sh """ ssh "${username}"@"${ipAdr}" sudo docker rm "${applicationName}" || true """
         sh """ ssh "${username}"@"${ipAdr}" sudo docker pull "${dockerRepo}"/"${applicationName}":$BUILD_NUMBER """
         sh """ scp "${WORKSPACE}@script/resources/pet-deployment.yaml" "${username}"@"${ipAdr}":/tmp """
+        sh """ scp "${WORKSPACE}@script/resources/pet-clinic-service.yaml" "${username}"@"${ipAdr}":/tmp """
         sh """ ssh "${username}"@"${ipAdr}" sudo kubectl delete -f /tmp/pet-deployment.yaml || true """
+        sh """ ssh "${username}"@"${ipAdr}" sudo kubectl delete -f /tmp/pet-clinic-service.yaml || true """
         sh """ ssh "${username}"@"${ipAdr}" sudo kubectl apply -f /tmp/pet-deployment.yaml || true """
+        sh """ ssh "${username}"@"${ipAdr}" sudo kubectl apply -f /tmp/pet-clinic-service.yaml || true """
    }
 }
 }
