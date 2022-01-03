df303cda4b744426dd510a9ef4e45f1486995a30
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2022-01-03 12:47:19 +0530

    Update Classs.groovy

--- src/org/foo/Classs.groovy
+++ src/org/foo/Classs.groovy
@@ -1,11 +1,11 @@
-class pet implements Serializable {
-  def pipeline
+class pet implements Serializable {         //Class Name is pet
+  def pipeline                              // defining ibject 
   
-  pet(pipeline){
-    this.pipeline = pipeline 
+  pet(pipeline){                            //assigning class into object 
+    this.pipeline = pipeline                //refering pipeline object using this "keyword"
   }
-  def HelloWorld(args){
-    pipeline.sh "echo Hello World ${args}"
+  def HelloWorld(args){                     //Helloworld is method 
+    pipeline.sh "echo Hello World ${args}"      // using object calling super class 
   }
 }
   
