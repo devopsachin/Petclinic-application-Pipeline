60d73aef83743a9fbcd51038594c26684a54fd07
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2022-01-03 10:56:41 +0530

    Update dockerUtility.groovy

--- src/org/foo/dockerUtility.groovy
+++ src/org/foo/dockerUtility.groovy
@@ -4,16 +4,10 @@ class Utility implements Serializable {     /* Library class  Serializable=this 
         this.steps = steps                  /* This keyword is used to refer current object instance */
     }
     def dockerLogin(String credentials){     /* dockerLogin is a Method */
-      try {
             this.steps.withCredentials([steps.usernamePassword(credentialsId: credentials, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD' )])   /* super class */
             {
                 this.steps.sh "sudo docker login -u " + '${USERNAME}' + " --password " + '${PASSWORD}' + " "
             }
-     } 
-       catch (error){
-           this.steps.echo error.getMessage()
-           throw error
-     }
     }
 
 
